dc9d0186012f2951e514997807de2cf220491747
Omar Alhammadi <emarati.no1@gmail.com>
2019-02-17 19:15:27 +0400

    init

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-ble-central.ble",
+    "file": "plugins/cordova-plugin-ble-central/www/ble.js",
+    "pluginId": "cordova-plugin-ble-central",
+    "clobbers": [
+      "ble"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-ble-central": "1.2.2"
+};
+// BOTTOM OF METADATA
+});
